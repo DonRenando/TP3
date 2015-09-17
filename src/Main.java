eade6cc8346542dabf3c227a571afa9f02d563f4
Quentin Rouland <quentin.roualnd.82@gmail.com>
2015-09-17 12:31:08 +0200

    lol

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,15 @@ public class Main {
     public static void main(String[] args) {
         ListeUtilisateursImpl userList = new ListeUtilisateursImpl();
         File f = new File("BD");
-
-        try {
-            userList.chargerListe(f);
-        }catch (Exception e) {
+        if (!f.exists()) {
             userList.ajouterUtilisateur(new Utilisateur("Durant", "Paul", Personne.ADMINISTRATEUR, "paul.durant", "tototo", ""));
             userList.ajouterUtilisateur(new Utilisateur("Dupont", "Jean", Personne.STATISTICIEN, "jean.dupont", "tototo", "paul.durant"));
+
             userList.sauverListe(f);
         }
+        else
+            userList.chargerListe(f);
+
         new FenetreIdentification(userList, f);
     }
 }
